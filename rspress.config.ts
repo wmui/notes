63308681646620bdb4d124a403d9b5d2e86045be
wmui <waynewang@primecare.group>
2024-09-23 16:07:55 +0800

    init

--- rspress.config.ts
+++ rspress.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: '胖胖熊笔记',
+  base: '/notes/',
   description: '记录我的学习笔记',
   icon: '/rspress-icon.png',
   logo: {
