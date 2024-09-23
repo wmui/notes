910392790cd004f4aa7e6c84578db88b46df1d35
wmui <waynewang@primecare.group>
2024-09-23 17:17:32 +0800

    add

--- docs/frame/react.md
+++ docs/frame/react.md
@@ -1,3 +1,3 @@
 # React
 
-## test
+## test react
--- rspress.config.ts
+++ rspress.config.ts
@@ -1,29 +1,13 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
-// import ghPages from 'rspress-plugin-gh-pages';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: '胖胖熊笔记',
-  base: '/',
   description: '记录我的学习笔记',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
-  // plugins: [
-  //   ghPages({
-  //     repo: 'https://github.com/wmui/notes.git',
-  //     branch: 'website',
-  //     siteBase:'/',
-  //     history: false,
-  //     cname: 'ppx.link'
-  //   }),
-  // ],
-  // themeConfig: {
-  //   socialLinks: [
-  //     { icon: 'github', mode: 'link', content: 'https://github.com/wmui' },
-  //   ],
-  // },
 });
